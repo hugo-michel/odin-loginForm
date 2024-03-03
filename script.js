f83e28ccd1dca9649a1acd71e820e98c8fab19d3
hugo-michel <orayito62@gmail.com>
2024-03-03 14:52:24 +0100

    ctrl maj i

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const SHOWPWD = document.querySelectorAll(".show-pwd");
 const PASSWORD = document.querySelector("#password");
-const PASSOWRDREPEAT = document.querySelector("#password-confirmation")
+const PASSOWRDREPEAT = document.querySelector("#password-confirmation");
 
-SHOWPWD.forEach(symbol => {
-    symbol.addEventListener("click", () => {
-        let input = symbol.nextElementSibling;
-        let currentType = input.getAttribute("type");
-        currentType === "password" ? input.setAttribute("type", "text") : input.setAttribute("type", "password");
-    })
-})
+SHOWPWD.forEach((symbol) => {
+	symbol.addEventListener("click", () => {
+		let input = symbol.nextElementSibling;
+		let currentType = input.getAttribute("type");
+		currentType === "password" ? input.setAttribute("type", "text") : input.setAttribute("type", "password");
+	});
+});
 
 //mdp equivalent
 
 function validatePassword() {
-    if (PASSWORD.value != PASSOWRDREPEAT.value) {
-        PASSOWRDREPEAT.setCustomValidity("Please enter the same password")
-    } else {
-        PASSOWRDREPEAT.setCustomValidity("")
-    }
+	if (PASSWORD.value != PASSOWRDREPEAT.value) {
+		PASSOWRDREPEAT.setCustomValidity("Please enter the same password");
+	} else {
+		PASSOWRDREPEAT.setCustomValidity("");
+	}
 }
 PASSWORD.addEventListener("keyup", validatePassword);
 PASSOWRDREPEAT.addEventListener("keyup", validatePassword);
